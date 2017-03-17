e713daf7d8e713debd5c836126c52a21358ec3f8
Christopher Coté <chris@entropealabs.com>
2017-03-17 15:47:56 -0500

    update for histograms

--- lib/rosetta_home_meteo_stick.ex
+++ lib/rosetta_home_meteo_stick.ex
@@ -1,5 +1,5 @@
 defmodule Cicada.DeviceManager.Device.WeatherStation.MeteoStick do
-  use GenServer
+  use Cicada.DeviceManager.DeviceHistogram
   require Logger
   alias Cicada.{DeviceManager}
   @behaviour Cicada.DeviceManager.Behaviour.WeatherStation
